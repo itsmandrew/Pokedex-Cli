ec7d8e6b3b751bdda250a984ca0d619aa0e0cc65
itsmandrew <theachang1688@gmail.com>
2025-05-08 16:25:52 -0700

    Made generic function for Fetch+Unmarshal

--- api/api.go
+++ api/api.go
@@ -15,80 +15,54 @@ const LOCATION_URL = "https://pokeapi.co/api/v2/location-area/"
 func GetLocationAreas(url string, cache *pk.Cache) (m.Config, error) {
 	var config m.Config
 
-	cacheData, cacheHit := cache.Get(url)
+	err := fetchAndUnmarshal(url, cache, &config)
 
-	var data []byte
-
-	if cacheHit {
-		fmt.Println("LOG --- Cache hit")
-		data = cacheData
-
-	} else {
-
-		fmt.Println("LOG --- Cache miss")
-
-		resp, err := http.Get(url)
-
-		if err != nil {
-			return m.Config{}, err
-		}
-
-		defer resp.Body.Close()
-
-		data, err = io.ReadAll(resp.Body)
-
-		if err != nil {
-			return m.Config{}, err
-		}
-		cache.Add(url, data)
-	}
-
-	if err := json.Unmarshal(data, &config); err != nil {
-		return m.Config{}, err
-	}
-
-	return config, nil
+	return config, err
 }
 
 func GetAreaPokemon(location string, cache *pk.Cache) (m.LocationArea, error) {
-
 	url := fmt.Sprintf("https://pokeapi.co/api/v2/location-area/%s/", location)
-
-	cacheData, cacheHit := cache.Get(url)
 	var area m.LocationArea
 
+	err := fetchAndUnmarshal(url, cache, &area)
+
+	return area, err
+}
+
+func fetchAndUnmarshal(url string, cache *pk.Cache, target any) error {
+
+	cacheData, cacheHit := cache.Get(url)
 	var data []byte
+	var err error
 
 	if cacheHit {
 		fmt.Println("LOG --- Cache hit")
 		data = cacheData
-
 	} else {
 		fmt.Println("LOG --- Cache miss")
 
 		resp, err := http.Get(url)
-
 		if err != nil {
-			return m.LocationArea{}, err
+			return err
 		}
 		defer resp.Body.Close()
 
 		if resp.StatusCode != http.StatusOK {
-			return m.LocationArea{}, fmt.Errorf("unexpected status: %s", resp.Status)
+			return fmt.Errorf("unexpected status: %s", resp.Status)
 		}
 
 		data, err = io.ReadAll(resp.Body)
 
 		if err != nil {
-			return m.LocationArea{}, err
+			return err
 		}
 
 		cache.Add(url, data)
 	}
 
-	if err := json.Unmarshal(data, &area); err != nil {
-		return m.LocationArea{}, err
+	if err = json.Unmarshal(data, &target); err != nil {
+		return err
 	}
 
-	return area, nil
+	return nil
 }
